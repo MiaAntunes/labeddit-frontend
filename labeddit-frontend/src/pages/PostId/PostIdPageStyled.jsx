da07f7b56bbdb9c54568946a19fdc4e4a8c19d1c
Camila Antunes <107724685+MiaAntunes@users.noreply.github.com>
2023-08-25 19:35:04 -0300

    Style: Implementando o css

---
+++ labeddit-frontend/src/pages/PostId/PostIdPageStyled.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const MainPostId = styled.main``
+
+export const SectionCreatePost = styled.section``
+
+export const FormCreatePost = styled.form``
+
+export const Line = styled.hr``
+
+export const SectionAllPost = styled.section``
